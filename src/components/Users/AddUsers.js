1b899a8056dd9e3d147c009fc8117089269f5ea0
shydleek <matiksh.2002@gmail.com>
2022-10-13 17:29:17 +0300

    feat: add useState hook and validation for input

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 
 import styles from './AddUsers.module.css';
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
+    }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     }
 
     return (
@@ -13,12 +35,12 @@ const AddUser = props => {
             <label htmlFor="username">
                 Username
             </label>
-            <input id="username" type="text"></input>
+            <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}></input>
             <label htmlFor="age">
                 Age (years)
             </label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add user</button>
+            <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
+            <Button type="submit">Add user</Button>
         </form>
     </Card>    
     );
